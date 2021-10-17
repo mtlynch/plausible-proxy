c450fdbc90ae71e0983fa52eecca83db8d26d813
Michael Lynch <mtlynch@users.noreply.github.com>
2021-10-17 17:20:34 -0400

    Use only a single cloud function for the proxy (#2)
    * Use only a single cloud function for the proxy
    
    * Balance brackets in JSON

--- dev-scripts/cli.go
+++ dev-scripts/cli.go
@@ -12,8 +12,7 @@ import (
 func main() {
 	var port = os.Getenv("PORT")
 	log.Print("starting up")
-	http.HandleFunc("/script", proxy.ProxyPlausibleScript)
-	http.HandleFunc("/event", proxy.ProxyPlausibleEvent)
+	http.HandleFunc("/", proxy.ProxyPlausible)
 	log.Printf("Listening on port %s", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,45 +1,75 @@
 package proxy
 
 import (
+	"errors"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strings"
 )
-
-const plausibleBaseURL = "https://plausible.io"
 
 var (
-	plausibleScriptURL = mustParseURL(plausibleBaseURL + "/js/plausible.js")
-	plausibleEventURL  = mustParseURL(plausibleBaseURL + "/api/event")
+	targetURL = mustParseURL("https://plausible.io")
 )
 
-func ProxyPlausibleScript(w http.ResponseWriter, r *http.Request) {
-	proxy := makePlausibleProxy(plausibleScriptURL)
-	proxy.ServeHTTP(w, r)
-}
+func ProxyPlausible(w http.ResponseWriter, r *http.Request) {
+	canonicalPath, err := canonicalizePath(r.URL.Path)
+	if err != nil {
+		log.Printf("path %s is not supported", r.URL.Path)
+		http.Error(w, "Unsupported path", http.StatusNotFound)
+		return
+	}
 
-func ProxyPlausibleEvent(w http.ResponseWriter, r *http.Request) {
-	proxy := makePlausibleProxy(plausibleEventURL)
-	proxy.ServeHTTP(w, r)
-}
-
-func makePlausibleProxy(targetURL *url.URL) *httputil.ReverseProxy {
 	proxy := httputil.NewSingleHostReverseProxy(targetURL)
 	proxy.Director = func(req *http.Request) {
 		req.URL = targetURL
+		req.URL.Path = canonicalPath
 		req.Host = targetURL.Host
 
 		req.Header.Add("X-Forwarded-Proto", req.Proto)
 		req.Header.Add("X-Forwarded-Host", req.Host)
 	}
-	return proxy
+	proxy.ServeHTTP(w, r)
+}
+
+func canonicalizePath(path string) (string, error) {
+	mappings := map[string]string{
+		"/api/event":       "/api/event",
+		"/js/plausible.js": "/js/plausible.js",
+		// Alias for plausible.js in case of ad-blockers who match the plausible.js
+		// string.
+		"/js/script.js": "/js/plausible.js",
+	}
+	for k, v := range mappings {
+		// Use HasSuffix instead of direct matching because there might be a prefix
+		// in the path, depending on how the client is forwarding to the proxy.
+		if strings.HasSuffix(path, k) {
+			return v, nil
+		}
+	}
+	return "", errors.New("unsupported path")
+}
+
+// Only proxy requests to a whitelist of paths that are necessary for loading
+// the plausible.js script and registering an event.
+func isWhitelistedPath(path string) bool {
+	whitelistedPaths := []string{
+		"/js/plausible.js",
+		"/api/event",
+	}
+	for _, p := range whitelistedPaths {
+		if p == path {
+			return true
+		}
+	}
+	return false
 }
 
 func mustParseURL(u string) *url.URL {
 	parsed, err := url.Parse(u)
 	if err != nil {
-		log.Printf("failed to parse %s", u)
+		log.Printf("failed to parse URL %s", u)
 		panic(err)
 	}
 	return parsed
