876fdcf3ad83aeed7f8f265627d26372477494ff
Michael Lynch <git@mtlynch.io>
2021-10-17 12:01:59 -0400

    Fix proxy

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,14 +1,23 @@
 package proxy
 
 import (
-	"io"
 	"log"
 	"net/http"
 	"net/http/httputil"
-	"os"
-	"time"
+	"net/url"
 )
 
+var target = mustParseURL("https://plausible.io/api/event")
+
 func ProxyRequest(w http.ResponseWriter, r *http.Request) {
-	httputil.NewSingleHostReverseProxy(url.Parse("https://plausible.io/api/event")).ServeHTTP(w, r)
+	httputil.NewSingleHostReverseProxy(target).ServeHTTP(w, r)
+}
+
+func mustParseURL(u string) *url.URL {
+	parsed, err := url.Parse(u)
+	if err != nil {
+		log.Printf("failed to parse %s", u)
+		panic(err)
+	}
+	return parsed
 }
