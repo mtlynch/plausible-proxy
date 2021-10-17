ecf36dee0109ae8ba0ec25e3bcf1026c7206a1b7
Michael Lynch <git@mtlynch.io>
2021-10-17 12:07:36 -0400

    Fix deployment

--- dev-scripts/cli.go
+++ dev-scripts/cli.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	var port = os.Getenv("PORT")
 	log.Print("starting up")
-	http.HandleFunc("/", proxy.ProxyRequest)
+	http.HandleFunc("/", proxy.ProxyPlausible)
 	log.Printf("Listening on port %s", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
